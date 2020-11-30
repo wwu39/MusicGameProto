2c9c93b76dc3422602e36fc8e747929b401da598
Wiorno K. Wiovanna <wwu39@ucsc.edu>
2020-11-29 17:29:59 -0800

    承上

---
+++ Assets/Utils/ScreenAdjust.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenAdjust : MonoBehaviour
+{
+    [SerializeField] CanvasScaler cs;
+    private void Awake()
+    {
+        cs = GetComponent<CanvasScaler>();
+        float curSrceenRatio = (float) Screen.width / Screen.height;
+        float defaultSrceenRatio = (float)DefRes.x / DefRes.y;
+        cs.matchWidthOrHeight = curSrceenRatio > defaultSrceenRatio ? 1 : 0;
+    }
+}
