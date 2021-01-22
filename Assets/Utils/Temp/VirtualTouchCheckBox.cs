c677e4c9bcb65732494035a34f8fcb41b1850b69
Wiorno K. Wiovanna <wwu39@ucsc.edu>
2021-01-22 03:44:15 -0800

    abc

--- Assets/Utils/Temp/VirtualTouchCheckBox.cs
+++ Assets/Utils/Temp/VirtualTouchCheckBox.cs
@@ -9,6 +9,9 @@ public class VirtualTouchCheckBox : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
+#if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
+        Destroy(gameObject);
+#endif
         checkBox.interactable = false;
     }
     void Start()
